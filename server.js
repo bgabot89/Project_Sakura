38cc54ed3fa7895c1770e699fc70457c6b33ad53
bgabot89 <bgabot89@gmail.com>
2016-10-25 21:42:28 -0700

    deploy to heroku

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+// var webpack = require('webpack');
+// var WebpackDevServer = require('webpack-dev-server');
+// var config = require('./webpack.config');
+//
+// new WebpackDevServer(webpack(config), {
+//   publicPath: config.output.publicPath,
+//   hot: true,
+//   historyApiFallback: true
+// }).listen(3000, 'localhost', function (err, result) {
+//   if (err) {
+//     return console.log(err);
+//   }
+//
+//   console.log('Listening at http://localhost:3000/');
+// });
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
 
-  console.log('Listening at http://localhost:3000/');
+app.use(express.static(__dirname));
+
+app.get('*', (req,res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
 });
+
+app.listen(port);
+console.log('Server started');
